0823cdd2e6d9c11d50853eb65e6c1979c0e26ebe
me-cbr <mecoelhobrocha@outlook.com>
2024-02-14 12:19:14 -0300

    urls e laung.json alterados

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('UMA LINDA STRING')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
