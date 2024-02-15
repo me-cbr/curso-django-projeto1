9c85cc474c39e823ecb4ad2aa60a4d64f035b38c
me-cbr <mecoelhobrocha@outlook.com>
2024-02-15 11:46:06 -0300

    Uma nova URL para uma nova View

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Maria Eduarda Coelho',
     })
 
-
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Maria Eduarda Coelho',
+    })
