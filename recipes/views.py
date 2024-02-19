0720b85be7f5c29ba190cec9cbfaca0690b1e78e
me-cbr <mecoelhobrocha@outlook.com>
2024-02-19 13:24:11 -0300

    Adicionando informações caso não tenha nenhuma recipe.

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published = True).order_by('-id')   
+    recipes = Recipe.objects.filter(
+        is_published = True,
+        ).order_by('-id')
+       
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
