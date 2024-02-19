6fe4fde8c69a534ca96b417886f76d21254b9294
me-cbr <mecoelhobrocha@outlook.com>
2024-02-19 09:18:50 -0300

    Adicionando coisas no admin do django e adicionando os staticfiles e os mediafiles no settings

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Category, Recipe
 
 
+
 class CategoryAdmin(admin.ModelAdmin):
     ...
-    
+
+@admin.register(Recipe)    
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
 
 admin.site.register(Category, CategoryAdmin)    
