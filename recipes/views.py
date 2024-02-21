9209cf786843f6a4cbf388d8a89b668a5cfdc85a
me-cbr <mecoelhobrocha@outlook.com>
2024-02-21 15:06:10 -0300

    Levantando erro 404 se o termo de busca não for enviado

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import Http404
 from recipes.models import Recipe
 
 
@@ -31,4 +32,10 @@ def recipe(request, id):
     })
 
 def search(request):
+    search_term = request.GET.get('q')
+    
+    
+    if not search_term:
+        raise Http404()
+    
     return render(request, 'recipes/pages/search.html')
